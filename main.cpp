beaca868e617eb271a3adf0c8aa9c22a17bcd477
Rogot <30789823+Rogot@users.noreply.github.com>
2021-04-24 23:39:26 +0700

    Video

--- main.cpp
+++ main.cpp
@@ -32,37 +32,51 @@ cv::Point maxLoc(0,0);
 int main()
 {
 	int num_ph = 31;
-	std::string file_name;
-	cv::Mat imgCrop;
+	std::string file_name, video_name;
+	video_name = "./Test_photo/ct_cam.mp4";
 
-	for (int i = 1; i <= num_ph; ++i)
-	{
-		file_name = "./Test_photo/ct" + std::to_string(i) + ".jpg";
+	cv::namedWindow("Cap", cv::WINDOW_AUTOSIZE);
 
-		image = imread(file_name, cv::IMREAD_COLOR);
+	// получаем информацию о видео-файле
+	cv::VideoCapture cap(video_name);
+	double fps = cap.get(cv::CAP_PROP_FPS);
 
-		find_QR(image);
-		find_tip(image);
+
+	while (cap.isOpened()) {
+		cv::Mat frame;
+		bool bSuccess = cap.read(frame);
+
+		//Breaking the while loop at the end of the video
+		if (bSuccess == false)
+		{
+			std::cout << "Found the end of the video" << std::endl;
+			break;
+		}
+
+		find_QR(frame);
+		find_tip(frame);
 		findRect(boundRectQR, boundRectTip);
-
-		//imgCrop = image(boundRectTemp[0]);
-
-		//find(imgCrop);
 
 		for (int i = 0; i < boundRectTemp.size(); ++i)
 		{
-			cv::rectangle(image, boundRectTemp[i].tl(), boundRectTemp[i].br(), cv::Scalar(0, 255, 0), 2);
+			cv::rectangle(frame, boundRectTemp[i].tl(), boundRectTemp[i].br(), cv::Scalar(0, 255, 0), 2);
 		}
 
-		imorientation(image, boundRectTemp);
+		imorientation(frame, boundRectTemp);
 
 		boundRectTemp.clear();
-		cv::imshow("origin " + std::to_string(i), image);
 
-		cv::waitKey(0);
-		cv::destroyAllWindows();
+		cv::imshow("Cap", frame);
+
+		if (cv::waitKey(10) == 27)
+		{
+			std::cout << "Esc key is pressed by user. Stoppig the video" << std::endl;
+			break;
+		}
+
 	}
-	cv::destroyAllWindows();
 
+	cv::destroyAllWindows();
+	
 	return 0;
 }
