b1357bcbe1910dcde5afd7c6f95321ec3d37593c
Rogot <30789823+Rogot@users.noreply.github.com>
2021-05-23 14:06:55 +0700

    1111

--- main.cpp
+++ main.cpp
@@ -21,101 +21,87 @@
 
 
 
-void find_robot(cv::Mat* frame)
+void find_robot(cv::Mat& frame)
 {
-	find_QR(*frame);
-	find_tip(*frame);
+	find_QR(frame);
+	find_tip(frame);
 	findRect(boundRectQR, boundRectTip);
 
 	for (int i = 0; i < boundRectTemp.size(); ++i)
 	{
-		cv::rectangle(*frame, boundRectTemp[i].tl(), boundRectTemp[i].br(), cv::Scalar(0, 255, 0), 2);
-		imorientation(*frame, boundRectTemp);
+		cv::rectangle(frame, boundRectTemp[i].tl(), boundRectTemp[i].br(), cv::Scalar(0, 255, 0), 2);
+		imorientation(frame, boundRectTemp);
 
 		boundRectTemp.clear();
-		cv::imshow("origin " + std::to_string(i), *frame);
+		cv::imshow("origin " + std::to_string(i), frame);
 
 		cv::waitKey(0);
 		cv::destroyAllWindows();
 	}
 }
 
-namespace us
+void func(cv::Mat& frame)
 {
-	void equalizeHist_BGR(const cv::Mat& input, cv::Mat& output) {
+	cv::Mat imgBlur, temp, imgCanny, imgCanny2, imgRes, imgDel;
+	int contrSize = 2500;
+	int errRate = 200;
+	cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
 
-		if (input.channels() < 3) return;
+	//us::makeBorder(frame, temp, 0.25);
 
-		std::vector<cv::Mat> channels;
-		cv::split(input, channels);
+	cv::imshow("original", frame);
 
-		cv::Mat R, G, B;
-		cv::equalizeHist(channels[0], B);
-		cv::equalizeHist(channels[1], G);
-		cv::equalizeHist(channels[2], R);
+	cv::resize(frame, imgRes, cv::Size(frame.cols * 0.5, frame.rows * 0.5));
 
-		std::vector<cv::Mat> combined;
-		combined.push_back(B);
-		combined.push_back(G);
-		combined.push_back(R);
-
-		cv::merge(combined, output);
-	}
+	cv::medianBlur(imgRes, imgBlur, 7);
+	cv::imshow("blurtemp,", imgBlur);
 
 
-	void func(cv::Mat& frame)
-	{
-		cv::Mat blur;
-		cv::Mat temp;
+	cv::Canny(imgRes, imgCanny, lowThreshold, lowThreshold * ratio, 3);
+	cv::imshow("imgCanny", imgCanny);
 
-		cv::imshow("original", frame);
+	us::equalizeHist_BGR(imgRes, frame);
+	cv::imshow("equalizeHist_BGR", frame);
 
-		us::equalizeHist_BGR(frame, blur);
-		cv::imshow("equalizeHist_BGR", blur);
+	//cv::namedWindow("Trackbars", (640, 200));
+	//cv::createTrackbar("Threshold", "Trackbars", &lowThreshold, 80);
 
-		cv::medianBlur(blur, frame, 3);
-		cv::imshow("equalizeHist_BGR + MedianFilter", frame);
+	//cv::medianBlur(frame, blur, 3);
 
+	//cv::dilate(temp, frame, kernel);
+	//cv::cvtColor(blur, frame, cv::COLOR_RGB2HSV);
 
-		//cv::namedWindow("Trackbars", (640, 200));
-		//cv::createTrackbar("Threshold", "Trackbars", &lowThreshold, 179);
+	/*
+	cv::namedWindow("Trackbars", (640, 200));
+	cv::createTrackbar("Hue Min", "Trackbars", &hmin, 179);
+	cv::createTrackbar("Hue Max", "Trackbars", &hmax, 179);
+	cv::createTrackbar("Sat Min", "Trackbars", &smin, 255);
+	cv::createTrackbar("Sat Max", "Trackbars", &smax, 255);
+	cv::createTrackbar("Val Min", "Trackbars", &vmin, 255);
+	cv::createTrackbar("Val Max", "Trackbars", &vmax, 255);
 
-		//cv::medianBlur(*frame, blur, 3);
+	cv::Scalar lower(hmin, smin, vmin);
+	cv::Scalar upper(hmax, smax, vmax);
 
-		//cv::Canny(blur, *frame, lowThreshold, lowThreshold * ratio, 3);
-		//cv::dilate(temp, *frame, kernel);
-		//cv::cvtColor(blur, *frame, cv::COLOR_RGB2HSV);
+	cv::inRange(imgRes, lower, upper, mask);
+	imgRes = mask;
 
-
-
-		/*
-		cv::namedWindow("Trackbars", (640, 200));
-		cv::createTrackbar("Hue Min", "Trackbars", &hmin, 179);
-		cv::createTrackbar("Hue Max", "Trackbars", &hmax, 179);
-		cv::createTrackbar("Sat Min", "Trackbars", &smin, 255);
-		cv::createTrackbar("Sat Max", "Trackbars", &smax, 255);
-		cv::createTrackbar("Val Min", "Trackbars", &vmin, 255);
-		cv::createTrackbar("Val Max", "Trackbars", &vmax, 255);
-
-		cv::Scalar lower(hmin, smin, vmin);
-		cv::Scalar upper(hmax, smax, vmax);
-
-		cv::inRange(*frame, lower, upper, mask);
-		*frame = mask;
-		*/
-	}
+	cv::imshow("HSV mask", imgRes);
+	*/
+	
 }
 
-
+#if 0
 int main()
 {
 	int num_ph = 31;
 	std::string file_name, video_name;
 	video_name = "./Test_photo/Second/Captured_file_2.avi";
 	file_name = "./Test_photo/ct1.jpg";
-
-	is_video(video_name, 10, *us::func, 0);
-	//is_photo(file_name, num_ph, *find_robot);
+	
+	us::isVideo(video_name, 10, func, 0);
+	//us::isPhoto(file_name, num_ph, find_robot);
 	
 	return 0;
 }
+#endif
